474638cb5d9ba4318846282af2d0d8d88dfa404d
Florent Biville <florent.biville@neo4j.com>
2022-08-12 14:27:58 +0200

    Update to latest version and download from Maven central

--- 0-install.sh
+++ 0-install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
-LIQUIBASE_NEO4J_VERSION="4.8.0.1"
+LIQUIBASE_NEO4J_VERSION="4.15.0"
 
 brew install --quiet liquibase
 
 export LIQUIBASE_HOME="$(brew --prefix liquibase)/libexec"
 
-plugin_full_jar="https://github.com/liquibase/liquibase-neo4j/releases/download/liquibase-neo4j-${LIQUIBASE_NEO4J_VERSION}/liquibase-neo4j-${LIQUIBASE_NEO4J_VERSION}-full.jar"
+plugin_full_jar="https://repo.maven.apache.org/maven2/org/liquibase/ext/liquibase-neo4j/${LIQUIBASE_NEO4J_VERSION}/liquibase-neo4j-${LIQUIBASE_NEO4J_VERSION}-full.jar"
 
 curl --silent \
 	--location "${plugin_full_jar}" \

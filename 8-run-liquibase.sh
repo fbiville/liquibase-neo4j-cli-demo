55d15060accb40497aff0dcb9908dc3a0691634f
Florent Biville <florent.biville@gmail.com>
2022-09-08 17:31:15 +0200

    Deduplicate Liquibase run script

--- 8-run-liquibase.sh
+++ 8-run-liquibase.sh
@@ -1,15 +1,1 @@
-#!/usr/bin/env bash
-set -Eeuxo pipefail
-
-PASSWORD=pass
-
-liquibase update \
-	--changelog-file ./changeLog.xml \
-	--url jdbc:neo4j:bolt://localhost \
-	--username neo4j \
-	--password "${PASSWORD}"
-
-liquibase history \
-	--url jdbc:neo4j:bolt://localhost \
-	--username neo4j \
-	--password "${PASSWORD}"
+3-run-liquibase.sh
